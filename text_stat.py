817303d5455d03eb74156c41126274c523cee390
Yuriy Golenkov <113295682+golenkovyura@users.noreply.github.com>
2023-10-12 21:16:27 +0300

    Update text_stat.py

--- text_stat.py
+++ text_stat.py
@@ -1,4 +1,6 @@
-import re
+from re import sub
+from string import ascii_lowercase
+from collections import Counter
 
 
 def text_stat(filename: str) -> dict:
@@ -7,7 +9,7 @@ def text_stat(filename: str) -> dict:
     """
     try:
         with open(filename, encoding='utf-8') as file:
-            text = file.read()
+            text = file.read().lower()
     except FileNotFoundError:
         return {'error': 'Файла с таким именем не существует.'}
     except ValueError:
@@ -15,16 +17,16 @@ def text_stat(filename: str) -> dict:
 
     result = {}
     # заполянем словарь ключами - буквами алфавитов
-    for key in map(chr, range(97, 123)):
+    for key in ascii_lowercase:
         result[key] = (0, 0)
-    for key in map(chr, range(1072, 1104)):
+    for key in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя':
         result[key] = (0, 0)
     # если буквы есть в тексте, то меняем значение по ключу в словаре
     for letter, value in words_with_letter(text).items():
         if letter in result.keys():
             result[letter] = value
     result['word_amount'] = word_amount(text)
-    result['paragraph_amount'] = paragraph_amount(text)
+    result['paragraph_amount'] = paragraph_amount(filename)
     result['bilingual_word_amount'] = bilingual_word_amount(text)
     return result
 
@@ -35,13 +37,13 @@ def bilingual_word_amount(text):
     букв кириллического и латинского алфавитов.
     """
     count = 0
-    for word in text.lower().split():
+    for word in text.split():
         russian_letter = False
         english_letter = False
         for symbol in word:
-            if 97 <= ord(symbol) <= 122:  # диапзаон маленьких английских букв в ASCII таблице
+            if symbol in ascii_lowercase:
                 english_letter = True
-            if 1072 <= ord(symbol) <= 1103:  # диапзаон маленьких русских букв в ASCII таблице
+            if symbol in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя':
                 russian_letter = True
         if english_letter and russian_letter:
             count += 1
@@ -51,10 +53,10 @@ def bilingual_word_amount(text):
 def word_amount(text):
     """
     Функция подсчета слов в тексте.
-    Словомм будем считать непустую строку, состоящую из букв латинского или
+    Словом будем считать непустую строку, состоящую из букв латинского или
     кирилического алфавита или отдельное число.
     """
-    text = re.sub(r'[^\w\s]', '', text)  # очищаем текст от знаков препинания 
+    text = sub(r'[^\w\s]', '', text)  # очищаем текст от знаков препинания
     return len([word for word in text.split() if word.isalpha() or
                 word.isalnum()])
 
@@ -63,11 +65,8 @@ def frequency_using_letter(text):
     """
     Функция подсчета кол-ва букв в тексте.
     """
-    text = re.sub(r'[^\w\s]', '', text)
-    result = {}
-    for word in text.split():
-        for letter in word.lower():
-            result[letter] = result.get(letter, 0) + 1
+    text = sub(r'[^\w\s]', '', text)
+    result = Counter(''.join(text.split()))
     return result
 
 
@@ -80,18 +79,33 @@ def words_with_letter(text):
     result = {}
     for key, value in frequency_using_letter(text).items():
         count = 0
-        for word in re.sub(r'[^\w\s]', '', text).split():
-            if key in word.lower():
+        for word in sub(r'[^\w\s]', '', text).split():
+            if key in word:
                 count += 1
-        value = (value, count / word_amount(text))
+        value = (value, round(count / word_amount(text), 3))
         result[key] = value
     return result
 
 
-def paragraph_amount(text):
+def paragraph_amount(filename):
     """
     Функция считающая кол-во абзацев в тексте.
-    Абзацем будем считать строку, начинающуюся с табуляции или
-    если между строками есть пустая, разедлительная строчка.
     """
-    return text.count('\n\n') + text.count('\n\t') + text.count('\n    ') + 1
+    count = 0
+    with open(filename, 'r', encoding='utf-8') as f:
+        rows = f.read().splitlines()
+        for row in rows:  # Ищем первую непустую строку
+            if len(row.strip()):
+                first_row = rows.index(row)
+                count += 1
+                break
+
+        array = rows[first_row + 1:]
+        for index in range(len(array)):  # Начиная с непустой строки перебираем строки и считаем абзацы
+            empty = False
+            if index >= 1 and len(array[index - 1].strip()) == 0:
+                empty = True
+            if ((array[index].startswith(('\t', ' ', '\n')) or empty) and
+                    len(array[index].strip())):
+                count += 1
+        return count
